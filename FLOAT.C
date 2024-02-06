459f4b6700b62a2dd4ad2a6f93f47f014d110245
Shreyas Wagire <122631659+ShreyasCyber@users.noreply.github.com>
2024-02-06 16:07:16 +0530

    Add files via upload

---
+++ FLOAT.C
@@ -0,0 +1,67 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	clrscr();
+	float item1,item2,no_item1,no_item2,average;
+
+	printf("\n\t Enter weight for item1:");
+	scanf("%f",&item1);
+
+	printf("\n\t enter no of item1:");
+	scanf("%f",&no_item1);
+
+	printf("\n\t Enter weight for item2:");
+	scanf("%f",&item2);
+
+	printf("\n\t enter no of item2:");
+	scanf("%f",&no_item2);
+
+	average=((item1*no_item1)+(item2*no_item2))/2;
+	printf("\n\t average=%f",average);
+
+	getch();
+
+
+}
+
+/*
+
+
+         Enter weight for item1:10                                              
+                                                                                
+         enter no of item1:10                                                   
+                                                                                
+         Enter weight for item2:20                                              
+                                                                                
+         enter no of item2:20                                                   
+                                                                                
+         average=250.000000                                                     
+
+ */
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+
+
+
+
+
+
+
+
+
+
+
+
+
