1b24e47f288201a4830dfc046be6277a22912176
Shreyas Wagire <122631659+ShreyasCyber@users.noreply.github.com>
2024-02-06 15:46:21 +0530

    Add files via upload

---
+++ AREA&PER.C
@@ -0,0 +1,52 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	clrscr();
+	int l,w,area,perimeter;
+
+	//user entering value of l
+	printf("\n\tenter l:");
+	scanf("%d",&l);
+
+	//user entering value of b
+	printf("\n\tenter w:");
+	scanf("%d",&w);
+
+	//area
+	area=l*w;
+	printf("\n\tarea is %d",area);
+
+	//perimeter
+	perimeter=2*(l+w);
+	printf("\n\tPerimeter is %d",perimeter);
+	getch();
+
+}
+
+/*
+
+	enter l:10
+
+	enter w:10
+
+	area is 100
+	Perimeter is 40
+
+*/
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+                                                                                
---
+++ NAME.C
@@ -0,0 +1,25 @@
+
+#include<conio.h> //console input output
+void main()
+{
+	clrscr();
+	printf("\n\n\t\t\tname:Shreyas Wagir");
+	printf("\n\n\t\t\tDOB: 25-10-2004");
+	printf("\n\n\t\t\tMobile:7499321428");
+	getch();
+}
+
+
+/*
+
+
+			name:Shreyas WagirA
+
+			DOB: 25-10-2004
+
+			Mobile:74993214
+
+
+
+*/
+
